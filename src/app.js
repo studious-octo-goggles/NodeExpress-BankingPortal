b213e3a5e0f81533577420a661da5567ff3f0124
Prateek <kumar.prateek@capgemini.com>
2020-01-23 14:01:10 +0530

    Module 4

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended:true}));
+
 
     const accountData = fs.readFileSync(
         path.join(__dirname, 'json', 'accounts.json'), 'utf8'
@@ -28,12 +30,36 @@ app.get('/', (req, res)  => {
 app.get('/savings',(req, res) => {
     res.render('account', { account: accounts.savings });
 });
+
 app.get('/checking',(req, res) => {
     res.render('account', { account: accounts.checking });
 });
+
 app.get('/credit',(req, res) => {
     res.render('account', { account: accounts.credit });
 });
+
+app.get('/transfer',(req, res) => res.render('transfer'));
+
+app.post('/transfer',(req, res) => {
+accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount,10);
+
+const accountsJSON = JSON.stringify(accounts, null, 4);
+fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+res.render('transfer', { message: 'Transfer Completed' });
+});
+
+app.get('/payment',(req, res) => res.render('payment', {account: accounts.credit}));
+
+app.post('/payment', (req,res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync( path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
+});
+
 app.get('/profile',(req, res) => {
     res.render('profile', { user: users[0] });
 });
